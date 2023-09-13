726e21eda3e1c694059df00d3f63d46af0a92fae
shagor7 <40910111+shagor7@users.noreply.github.com>
2023-09-14 00:40:00 +0600

    done with bit++
    it took very long!

--- 282A-Bit++.c
+++ 282A-Bit++.c
@@ -1,13 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <string.h>
 
-int main()
-{
+int main() {
     int n = 0, i;
-    char op[3];
     scanf("%d", &n);
-    for(i = 0; i<n : i ++)
-    {
-        scanf("%s", &op);
-        if(i = 0; i <  )
+
+    int x = 0;
+    char operation[4];  // To store the operation, including null terminator '\0'
+
+    for ( i = 0; i < n; i++) {
+        scanf("%s", operation);
+
+        if (strcmp(operation, "++x") == 0 || strcmp(operation, "x++") == 0) {
+            x++;
+        } else if (strcmp(operation, "--x") == 0 || strcmp(operation, "x--") == 0) {
+            x--;
+        }
     }
+
+    printf("%d\n", x);
+
+    return 0;
 }
