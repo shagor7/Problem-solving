217d39b82ad50d91be90f8c5a31846edac9c4295
shagor7 <40910111+shagor7@users.noreply.github.com>
2023-09-21 00:21:34 +0600

    some change

--- 112A-Petya and Strings.c
+++ 112A-Petya and Strings.c
@@ -7,4 +7,12 @@ int main ()
     scanf("%c%c", a,b);
 
     for(i = 0; a == '\0' && b == '\0'; i++)
+    {
+        if(a[i]==b[i] || a[i] + '65' == b[i] || a[i] == b[i] + '65' && a[i] == '\0' && b[i] == '\0')
+        {
+            printf("%d", 0);
+            break;
+        }
+        if(a[i] > b[i] || )
+    }
 }
